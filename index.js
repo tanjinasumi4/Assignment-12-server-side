a170f091d4dff8979876da88576667ede8e66ac6
tanjinasumi4 <tanjinasumi0@gmail.com>
2021-11-13 22:37:45 +0600

    last commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const { MongoClient } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
-const serviceAccount = require('./glamour-world-firebase-adminsdk.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
